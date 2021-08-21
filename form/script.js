6bbc419366ad6fce23452c344bd86def0b5b9fec
HAT8D5YTYUT <djeninbousidibe@gmail.com>
2021-08-21 08:38:11 +0000

    form

---
+++ form/script.js
@@ -0,0 +1,15 @@
+ function changeconteneurparagraphe(){
+
+
+var contenermail= document.getElementById('email').value;
+var contenermotdepasse = document.getElementById('passe').value;
+
+if (contenermail != '' && contenermotdepasse != '') {
+
+	alert("valider le formulaire");
+} else{
+
+	alert("veuiller remplir tout les champs!");
+} 
+
+} 
